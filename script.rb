4d3d0aaf16e12ad2beede54c3c5e8ec4b631dd06
James Dabbs <jamesdabbs@gmail.com>
2016-06-02 11:35:20 -0400

    Package up api service object

--- script.rb
+++ script.rb
@@ -1,25 +1,52 @@
 require 'httparty'
 require 'pry'
 
-Url = "https://tiy-sinatra-todo.herokuapp.com"
+class MyApi
+  attr_reader :headers, :url
+
+  def initialize url
+    @url = url
+  end
+
+  def login_as username
+    @headers = { "Authorization" => username }
+  end
+
+  def list_items
+    HTTParty.get "#{url}/list",
+      headers: headers
+  end
+
+  def add_new_item title
+    HTTParty.post "#{url}/list",
+      body: { title: title }.to_json,
+      headers: headers
+  end
+
+  def mark_complete title
+    HTTParty.patch "#{url}/list",
+      query: { title: title },
+      headers: headers
+  end
+end
+
+api = MyApi.new "https://tiy-sinatra-todo.herokuapp.com"
+# api = MyApi.new "http://localhost:4567"
 
 puts "Welcome to the TODO manager"
 puts "What is your username? > "
 # username = gets.chomp
-username = "jdabbs"
+api.login_as "jdabbs"
 
-r = HTTParty.get "#{Url}/list",
-  headers: { "Authorization" => username }
+binding.pry
 
-puts "Your current todo list"
-r.each do |item|
-  puts "* #{item}"
-end
-
-puts "What would you like to add?"
-title = gets.chomp
-r = HTTParty.post "#{Url}/list",
-  body: { title: title }.to_json,
-  headers: { "Authorization" => username }
+# puts "Your current todo list"
+# api.list_items.each do |item|
+#   puts "* #{item}"
+# end
+#
+# puts "What would you like to add?"
+# title = gets.chomp
+# api.add_new_item title
 
 puts "Done"
